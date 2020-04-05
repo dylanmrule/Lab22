c35247febe60327bcb142ea1e5fdb862612f0780
dylanmrule <58126323+dylanmrule@users.noreply.github.com>
2020-04-05 17:16:32 -0400

    First commit

---
+++ IntroToMVC/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using IntroToMVC.Models;
+
+namespace IntroToMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Result(string Username, string Email, string Password, string Gender, string Color)
+        {
+            Person p = new Person();
+            p.Username = Username;
+            p.Email = Email;
+            p.Password = Password;
+            p.Gender = Gender;
+            p.Color = Color;
+            return View(p);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
